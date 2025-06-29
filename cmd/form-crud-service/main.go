026adcd70d1b7580e7bcf9619fda97cada6871fd
Osamikoyo <v.pylayev@mail.ru>
2025-06-29 08:05:51 +0500

    update main.go

--- cmd/form-crud-service/main.go
+++ cmd/form-crud-service/main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"context"
+	"os"
+	"os/signal"
+
+	"github.com/Koyo-os/form-crud-service/internal/app"
+	"github.com/Koyo-os/form-crud-service/internal/repo"
+)
+
+func getRepo() *repo.Repository {
+	
+}
+
 func main() {
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+
+
 	
 }
