60a31ad6b4ffb45bd7cf568c98266978ea3e61bd
Osamikoyo <v.pylayev@mail.ru>
2025-06-30 08:43:31 +0500

    update main.go

--- cmd/form-crud-service/main.go
+++ cmd/form-crud-service/main.go
@@ -2,22 +2,77 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"os"
 	"os/signal"
+	"time"
 
 	"github.com/Koyo-os/form-crud-service/internal/app"
 	"github.com/Koyo-os/form-crud-service/internal/repo"
+	"github.com/Koyo-os/form-crud-service/internal/server"
+	"github.com/Koyo-os/form-crud-service/internal/service"
+	"github.com/Koyo-os/form-crud-service/pkg/logger"
+	"go.uber.org/zap"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
-func getRepo() *repo.Repository {
-	
+func getRepo(logger *logger.Logger) (*repo.Repository, error) {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+	)
+
+	var (
+		err error
+		db  *gorm.DB
+	)
+
+	for range 7 {
+		db, err = gorm.Open(mysql.Open(dsn))
+		if err == nil {
+			break
+		} else {
+			logger.Error("failed connect to db",
+				zap.Error(err),
+				zap.String("db_user", os.Getenv("DB_USER")),
+				zap.String("db_password", os.Getenv("DB_PASSWORD")),
+				zap.String("db_host", os.Getenv("DB_HOST")),
+				zap.String("db_port", os.Getenv("DB_PORT")),
+				zap.String("db_name", os.Getenv("DB_NAME")))
+		}
+
+		time.Sleep(5 * time.Second)
+	}
+
+	if err != nil{
+		logger.Error("final fail connect to db", zap.Error(err))
+
+		return nil, err
+	}
+
+	return repo.NewRepository(db, logger), nil
 }
 
 func main() {
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
 	defer cancel()
 
+	repo, err := getRepo(logger.Get())
+	if err != nil{
+		return
+	}
 
+	service := service.NewService(repo)
 
-	
+	server := server.NewServer(service)
+
+	app := app.NewApp(server)
+
+	if err = app.Start(ctx);err != nil{
+		return
+	}
 }
--- go.mod
+++ go.mod
@@ -3,12 +3,14 @@ module github.com/Koyo-os/form-crud-service
 go 1.24.2
 
 require (
+	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/bytedance/sonic v1.13.3 // indirect
 	github.com/bytedance/sonic/loader v0.2.4 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/cloudwego/base64x v0.1.5 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/go-sql-driver/mysql v1.8.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
@@ -28,5 +30,6 @@ require (
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250324211829-b45e905df463 // indirect
 	google.golang.org/grpc v1.73.0 // indirect
 	google.golang.org/protobuf v1.36.6 // indirect
+	gorm.io/driver/mysql v1.6.0 // indirect
 	gorm.io/gorm v1.30.0 // indirect
 )
